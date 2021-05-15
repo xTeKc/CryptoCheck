302f69077476a3fc63bc884a447e8f5153f9da76
xTeKc <bcd3v@pm.me>
2021-05-15 16:04:29 -0400

    modified data

--- app/CryptoAPIs.hs
+++ app/CryptoAPIs.hs
@@ -1,8 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Network.HTTP.Base
+import qualified Data.ByteString.Lazy.Char8 as L8
 
-
-import 
-
-module CryptoAPIs (
+{-module CryptoAPIs (
 	btcAPI,
 	ethAPI,
 	adaAPI,
@@ -10,16 +10,14 @@ module CryptoAPIs (
 	xrpAPI,
 	dotAPI,
 	uniAPI
-	) where
+	) where -}
 
-btcAPI :: 
+cryptoAPIs =
 
-ethAPI ::
-
-adaAPI ::
-
-xrpAPI :: 
-
-dotAPI :: 
-
-uniAPI :: 
+		 btcAPI = "https://api.coingecko.com/api/v3/simple/price?ids=btc&vs_currencies=usd" >>= L8.putStrLn
+		 ethAPI = "https://api.coingecko.com/api/v3/simple/price?ids=eth&vs_currencies=usd" >>= L8.putStrLn
+		 adaAPI = "https://api.coingecko.com/api/v3/simple/price?ids=ada&vs_currencies=usd" >>= L8.putStrLn
+		 xrpAPI = "https://api.coingecko.com/api/v3/simple/price?ids=xrp&vs_currencies=usd" >>= L8.putStrLn
+		 dotAPI = "https://api.coingecko.com/api/v3/simple/price?ids=dot&vs_currencies=usd" >>= L8.putStrLn
+		 uniAPI = "https://api.coingecko.com/api/v3/simple/price?ids=uni&vs_currencies=usd" >>= L8.putStrLn
+		
