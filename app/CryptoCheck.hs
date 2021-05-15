33a7c035f032df665a7ee894cd87dc22e5aac1c0
xTeKc <bcd3v@pm.me>
2021-05-15 11:06:49 -0400

    refactorish

--- app/CryptoCheck.hs
+++ app/CryptoCheck.hs
@@ -15,29 +15,46 @@ main = putStrLn "Loading..."
 
 data Crypto = Crypto { cryptoName   :: String,
                        cryptoSymbol :: String,
-                       cryptoPrice  :: String
+                       cryptoPrice  :: [Char]
                          } deriving (Eq, Show)
+btcAPI :: 
+btcAPI = "https://api.coingecko.com/api/v3/simple/price?ids=btc&vs_currencies=usd"
+
+ethAPI ::
+ethAPI = "https://api.coingecko.com/api/v3/simple/price?ids=eth&vs_currencies=usd"
+
+adaAPI ::
+adaAPI = "https://api.coingecko.com/api/v3/simple/price?ids=ada&vs_currencies=usd"
+
+xrpAPI :: 
+xrpAPI = "https://api.coingecko.com/api/v3/simple/price?ids=xrp&vs_currencies=usd"
+
+dotAPI :: 
+dotAPI = "https://api.coingecko.com/api/v3/simple/price?ids=dot&vs_currencies=usd"
+
+uniAPI :: 
+uniAPI = "https://api.coingecko.com/api/v3/simple/price?ids=uni&vs_currencies=usd"
 
 bitcoin = Crypto {cryptoName = "Bitcoin", cryptoSymbol = "BTC", 
-cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=btc&vs_currencies=usd" }
+cryptoPrice = btcAPI }
 btcData = show bitcoin
 
 ethereum = Crypto {cryptoName = "Ethereum", cryptoSymbol = "ETH", 
-cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=eth&vs_currencies=usd" }
+cryptoPrice = ethAPI }
 ethData = show ethereum
 
 cardano = Crypto {cryptoName = "Cardano", cryptoSymbol = "ADA", 
-cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=ada&vs_currencies=usd" }
+cryptoPrice = adaAPI }
 adaData = show cardano
 
 xrp = Crypto {cryptoName = "XRP", cryptoSymbol = "XRP", 
-cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=xrp&vs_currencies=usd" }
+cryptoPrice = xrpAPI }
 xrpData = show xrp
 
 polkadot = Crypto {cryptoName = "Polkadot", cryptoSymbol = "DOT", 
-cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=dot&vs_currencies=usd" }
+cryptoPrice = dotAPI }
 dotData = show polkadot
 
 uni = Crypto {cryptoName = "Uni", cryptoSymbol = "UNI", 
-cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=uni&vs_currencies=usd" }
+cryptoPrice = uniAPI }
 uniData = show uni
