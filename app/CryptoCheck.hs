ac0bf1a39e5b675f05d7b188b7748f4e9d77d55a
xTeKc <bcd3v@pm.me>
2021-05-15 10:47:40 -0400

    renaming

--- app/CryptoCheck.hs
+++ app/CryptoCheck.hs
@@ -19,25 +19,25 @@ data Crypto = Crypto { cryptoName   :: String,
                          } deriving (Eq, Show)
 
 bitcoin = Crypto {cryptoName = "Bitcoin", cryptoSymbol = "BTC", 
-cryptoPrice = "" }
+cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=btc&vs_currencies=usd" }
 btcData = show bitcoin
 
 ethereum = Crypto {cryptoName = "Ethereum", cryptoSymbol = "ETH", 
-cryptoPrice = "" }
+cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=eth&vs_currencies=usd" }
 ethData = show ethereum
 
 cardano = Crypto {cryptoName = "Cardano", cryptoSymbol = "ADA", 
-cryptoPrice = "" }
+cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=ada&vs_currencies=usd" }
 adaData = show cardano
 
 xrp = Crypto {cryptoName = "XRP", cryptoSymbol = "XRP", 
-cryptoPrice = "" }
+cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=xrp&vs_currencies=usd" }
 xrpData = show xrp
 
 polkadot = Crypto {cryptoName = "Polkadot", cryptoSymbol = "DOT", 
-cryptoPrice = "" }
+cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=dot&vs_currencies=usd" }
 dotData = show polkadot
 
 uni = Crypto {cryptoName = "Uni", cryptoSymbol = "UNI", 
-cryptoPrice = "" }
+cryptoPrice = "https://api.coingecko.com/api/v3/simple/price?ids=uni&vs_currencies=usd" }
 uniData = show uni
