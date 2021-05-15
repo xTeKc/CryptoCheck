3bb0c350e7f73a768438fc86af52be55c0f87fb0
xTeKc <bcd3v@pm.me>
2021-05-15 11:09:56 -0400

    refactorish

--- app/CryptoCheck.hs
+++ app/CryptoCheck.hs
@@ -17,23 +17,7 @@ data Crypto = Crypto { cryptoName   :: String,
                        cryptoSymbol :: String,
                        cryptoPrice  :: [Char]
                          } deriving (Eq, Show)
-btcAPI :: 
-btcAPI = "https://api.coingecko.com/api/v3/simple/price?ids=btc&vs_currencies=usd"
 
-ethAPI ::
-ethAPI = "https://api.coingecko.com/api/v3/simple/price?ids=eth&vs_currencies=usd"
-
-adaAPI ::
-adaAPI = "https://api.coingecko.com/api/v3/simple/price?ids=ada&vs_currencies=usd"
-
-xrpAPI :: 
-xrpAPI = "https://api.coingecko.com/api/v3/simple/price?ids=xrp&vs_currencies=usd"
-
-dotAPI :: 
-dotAPI = "https://api.coingecko.com/api/v3/simple/price?ids=dot&vs_currencies=usd"
-
-uniAPI :: 
-uniAPI = "https://api.coingecko.com/api/v3/simple/price?ids=uni&vs_currencies=usd"
 
 bitcoin = Crypto {cryptoName = "Bitcoin", cryptoSymbol = "BTC", 
 cryptoPrice = btcAPI }
